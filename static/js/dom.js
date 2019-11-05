bf34a97ef7447e4cbaed1241f2a3c6a51a40d970
botond <belgianmaniax@gmail.com>
2019-11-06 00:15:39 +0100

    rearrange dom functions

--- static/js/dom.js
+++ static/js/dom.js
@@ -1,34 +1,52 @@
 // It uses data_handler.js to visualize elements
 import {dataHandler} from "./data_handler.js";
 
+function renderBoard(boardData, template) {
+    const board = document.importNode(template.content, true);
+    board.querySelector('.board-title').textContent = boardData.title;
+    return board
+}
+
+function handleCreateBoardInputClickOutside() {
+    const input = document.querySelector('input.create-board');
+    if (input.value) {
+        input.value = '';
+    }
+}
+
+function handleCreateBoardButtonClick() {
+    const form = document.querySelector('.form.create-board');
+    form.classList.toggle('hidden');
+}
+
+function handleSaveBoardButtonClick() {
+    const boardTitle = document.querySelector('input.create-board').value;
+    if (!boardTitle) {
+        return;
+    }
+    dataHandler.createNewBoard(boardTitle, (boardData) => {
+        const boardTemplate = document.getElementById('board-template');
+        const board = renderBoard(boardData, boardTemplate);
+        const container = document.getElementById('boards');
+        container.appendChild(board);
+        const form = document.querySelector('.form.create-board');
+        form.querySelector('input').value = '';
+        form.classList.toggle('hidden');
+    })
+}
+
 export let dom = {
     init: function () {
         // This function should run once, when the page is loaded.
-        window.addEventListener('click', () => {
-            const input = document.querySelector('input.create-board');
-            if (input.value) {
-                input.value = '';
-            }
-        });
-        document.querySelector('button.create-board').addEventListener('click', () => {
-            const form = document.querySelector('.form.create-board');
-            form.classList.toggle('hidden');
-        });
-        document.querySelector('button.save-new-board').addEventListener('click', () => {
-            const boardTitle = document.querySelector('input.create-board').value;
-            if (!boardTitle) {
-                return;
-            }
-            dataHandler.createNewBoard(boardTitle, (boardData) => {
-                const boardTemplate = document.getElementById('board-template');
-                const board = this.renderBoard(boardData, boardTemplate);
-                const container = document.getElementById('boards');
-                container.appendChild(board);
-                const form = document.querySelector('.form.create-board');
-                form.querySelector('input').value = '';
-                form.classList.toggle('hidden');
-            })
-        })
+
+        // Get relevant elements
+        const createBoardButton = document.querySelector('button.create-board');
+        const saveBoardButton = document.querySelector('button.save-new-board');
+
+        // Add event listeners
+        window.addEventListener('click', handleCreateBoardInputClickOutside);
+        createBoardButton.addEventListener('click', handleCreateBoardButtonClick);
+        saveBoardButton.addEventListener('click', handleSaveBoardButtonClick);
     },
     loadBoards: function () {
         // retrieves boards and makes showBoards called
@@ -42,7 +60,7 @@ export let dom = {
         const boardTemplate = document.getElementById('board-template');
         const container = document.getElementById('boards');
         for (const boardData of boards) {
-            const board = this.renderBoard(boardData, boardTemplate);
+            const board = renderBoard(boardData, boardTemplate);
             container.appendChild(board);
         }
     },
@@ -54,9 +72,4 @@ export let dom = {
         // it adds necessary event listeners also
     },
     // here comes more features
-    renderBoard: function (boardData, template) {
-        const board = document.importNode(template.content, true);
-        board.querySelector('.board-title').textContent = boardData.title;
-        return board
-    }
 };
