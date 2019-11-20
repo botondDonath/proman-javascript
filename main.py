2cd9e1f4af010d4971d656a2e84d018f6f8adea6
Rajnai Máté <ramate.rajnai@gmail.com>
2019-11-20 12:05:49 +0100

    Create client-server connection for reorder cards

--- main.py
+++ main.py
@@ -119,6 +119,17 @@ def register_user():
     return data_manager.insert_user(user_data)
 
 
+@app.route('/reorder-cards', methods=['POST'])
+@json_response
+def reorder_cards():
+    BOARD_ID = 0
+    cards_data = request.get_json()
+    board_id = cards_data[BOARD_ID]['board_id']
+    data_manager.update_cards_order(cards_data)
+    updated_cards = data_manager.get_cards_for_board(board_id)
+    return updated_cards
+
+
 def main():
     app.run(debug=True)
 
